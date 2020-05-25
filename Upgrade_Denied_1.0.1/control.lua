b6bc45cdfa5cce62aa99b3f49a54f68d5ec1ccc8
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-05-25 17:15:30 +0200

    Update border checkpoint

--- Upgrade_Denied_1.0.1/control.lua
+++ Upgrade_Denied_1.0.1/control.lua
@@ -1,3 +1,8 @@
+print("Congratulations.")
+print("The October labor lottery is complete. Your name was pulled.")
+print("For immediate placement, report to the Ministry of Admission at Grestin Border Checkpoint.")
+print("An apartment will be provided for you and your family in East Grestin. Expect a Class-8 dwelling.")
+print("Glory to Arstotzka.")
 script.on_event(defines.events.on_marked_for_upgrade,
         function(event)
             if(event.entity.force.recipes[event.target.name]) then
@@ -7,3 +12,4 @@ script.on_event(defines.events.on_marked_for_upgrade,
             end
         end
 )
+
